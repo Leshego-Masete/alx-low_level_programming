d1ce42cd6fd344220bcdfe52b826c9766c1f1de6
Leshego-Masete <kmleshegpo2@gmail.com>
2022-10-16 16:19:17 -0700

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main entry point of the program
+ *
+ * Return: Always zero if no errors or non zero value
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+}
