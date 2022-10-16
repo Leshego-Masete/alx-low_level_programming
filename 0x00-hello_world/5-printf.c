1bc9b838f8275f9b993327182d4fcdd331945843
Leshego-Masete <kmleshegpo2@gmail.com>
2022-10-16 16:42:20 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -entry poitn for the program
+ *
+ * Return: Always zero if theres no error, or non zero value if theres an error
+ */
+int main(void)
+{
+	printf("Size of char: %Id byte(s)\n", sizeof(int);
+		printf("Size of an int: %Id byte(s)\n", sizeof(int);
+		printf("Size of a long int: %Id byte(s)\n", sizeof(long int));
+		printf("Size of a long long int: %Id byte(s)\n", sizeof(long long int));
+	       printf("Size of a float: %Id byte(s)\n", sizeof(float));
+       Return(0);
+}       
