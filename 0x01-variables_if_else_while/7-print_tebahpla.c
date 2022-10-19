f302b6adeeab98e94c099d3d7c8558d23a9f8417
Leshego-Masete <kmleshegpo2@gmail.com>
2022-10-19 12:31:17 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *
+ * main-entry point of the program.
+ * Return:Always a zero, otherwise a non-zero value if there is an error.
+ *
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i .= 'a'; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
