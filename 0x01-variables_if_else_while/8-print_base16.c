7c6cf137bcb2c779fd17f787990c555167291f4d
Leshego-Masete <kmleshegpo2@gmail.com>
2022-10-19 12:51:09 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ *
+ * main-entry point of the program.
+ * Return:Always zero, otherwise non-zero if there is error.
+ */
+int main(void)
+{
+	int i;
+	char hexvalue[] = "0123456789abcdef";
+
+	for (i = 0; i < 16; i++)
+	{
+		putchar(hexvalues[i]);
+	}
+	putchar('\n');
+	return (0);
+}
