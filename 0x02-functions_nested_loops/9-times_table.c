fa89f7136393dbe36f0e20c5509fa6c6fa50e7e3
Leshego-Masete <kmleshegpo2@gmail.com>
2022-10-20 03:07:15 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,10 +15,13 @@ void times_table(void)
 	for (a = 0; a <= 9; a++)
 	{
 		_putchar(48);
+		for (b = 1; b <= 9; b++)
+		{
+			op = a * b;
+			_putchar(44);
+			_putchar(32);
 
-		_putchar(32);
-
-		if(op <=9)
+	       if (op <=9)
 		{
 			_putchar(32);
 			_putchar(op + 48);
