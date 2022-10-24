aebe0ceca7649144968e2673d095953d94198594
Leshego-Masete <kmleshegpo2@gmail.com>
2022-10-24 04:13:17 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,15 +12,11 @@ int main(void)
 	int i;
 
 	printf("infinite loop incoming :(\n");
-
 	i = 0;
-
-	/* while (i < 10)*/
-	/*{*/
-	/*putchar(i);*/
-	 /*}*/
-
+	while (i < 10)
+	{
+	1++ ;	/*putchar(i);*/
+	}
 	printf("infinite loop avoided! \\o/\n");
-
 	return (0);
 }
