94b68c31ca295c295676e9ae61575874c1404014
Leshego-Masete <kmleshegpo2@gmail.com>
2022-10-25 05:39:48 -0700

    0-isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isupper-check if parameter is an uppercase character.
+ * _isupper-checks if parameter is an uppercase character.
  * @c:input character.
- * Return:1 if is an uppercase character, 0 in the other case.
+ * Return:1 if is an uppercase character, 0 in other case.
  */
 int _isupper(int c)
 {
@@ -11,8 +11,8 @@ int _isupper(int c)
 	{
 		return (1);
 	}
-		else
-		{
-			return (0);
-		}
+	else
+	{
+		return (0);
+	}
 }
