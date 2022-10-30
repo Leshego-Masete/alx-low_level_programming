d0ae6e0af7c5cc8fcb88baf42b9e1b84505b92f0
Leshego-Masete <kmleshegpo2@gmail.com>
2022-10-30 08:13:10 -0700

    more nested loop commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,9 @@
+#ifndef main_h
+#define main_h
+
 int _putchar(char c);
 int _isupper(int c);
+int _islower(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
@@ -9,3 +13,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);
+
+#endif
