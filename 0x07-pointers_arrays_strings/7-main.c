09caa7388604076a07d1107f018da186eeea6726
Leshego-Masete <kmleshegpo2@gmail.com>
2022-11-03 10:16:39 -0700

    Chess is mental torture

--- 0x07-pointers_arrays_strings/7-main.c
+++ 0x07-pointers_arrays_strings/7-main.c
@@ -14,7 +14,7 @@ int main(void)
 {
 	char board[8][8] = {
 		{'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
-		{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'}
+		{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
 		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
 		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
 		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
@@ -22,7 +22,6 @@ int main(void)
 		{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
 		{'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
 	};
-	print_chessboard(board)
+	print_chessboard(board);
 		return (0);
-
 }
